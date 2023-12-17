29d6ab9cb7d77d5ecfe51b1dac7c7fb430472b75
Muhammed Ali Öztürk <Muhammed_Ali95@outlook.com.tr>
2023-12-17 14:03:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,63 @@
+const githubForm = document.getElementById("github-form");
+const nameInput = document.getElementById("githubname");
+const ClearLastUsers = document.getElementById("clear-last-users");
+const lastUsers = document.getElementById("last-users");
+const github =new Github();
+const ui = new UI();
+
+eventListeners();
+
+function eventListeners()
+{
+    githubForm.addEventListener("submit",getData);
+    ClearLastUsers.addEventListener("click",clearAllSearched);
+    document.addEventListener("DOMContentLoaded",getAllSearched);
+
+}
+function getData(e)
+{
+     let username = nameInput.value.trim();
+     if(username === "")
+     {
+        alert("Lütfen geçerli bir kullanıcı girin.");
+     }else{
+        github.getGithubData(username)
+        .then(response => {
+            if(response.user.message === "Not Found")
+            {
+                ui.showError("kullanıcı bulunamadı!");
+            }
+            else{
+                ui.addSearchedUsersToUI(username);
+                Storage.addSearchedUsersToStorage(username);
+                ui.showUserInfo(response.user);
+                ui.showRepoInfo(response.repo);
+            }
+        })
+        .catch(err => ui.showError(err));
+     }
+
+     ui.clearInput();
+     e.preventDefault();
+}
+function clearAllSearched()
+{
+    if(confirm("emin misiniz?"))
+    {
+        Storage.clearSearchedUsersFromStorage();
+        ui.clearSearchedUsersFromUI();
+    }
+}
+function getAllSearched()
+{
+    let users = Storage.getSearchedUsersFromStorage();
+
+    let result = "";
+    users.forEach(user =>{
+
+        result +=  '<li class="list-group-item">'+user+'</li>';
+
+    });
+
+    lastUsers.innerHTML = result;
+}
